044628ba31ffd8fece70cd7434527cbd65aa05f8
mikail-khan <mikail.khan@digital.hmrc.gov.uk>
2021-01-07 10:44:30 +0000

    CDSRC-32: fix test

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,6 +2,7 @@ import sbt._
 
 object AppDependencies {
   val silencerVersion = "1.7.0"
+
   val compile         = Seq(
     "uk.gov.hmrc"    %% "bootstrap-backend-play-27" % "3.2.0",
     compilerPlugin("com.github.ghik" % "silencer-plugin" % silencerVersion cross CrossVersion.full),
@@ -9,7 +10,7 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "org.scalatest" %% "scalatest" % "3.2.3" % Test,
-    "org.pegdown"    % "pegdown"   % "1.6.0" % "test"
+    "org.scalatest" %% "scalatest" % "3.0.8" % Test,
+    "org.pegdown"    % "pegdown"   % "1.6.0" % Test
   )
 }
