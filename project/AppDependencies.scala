aa9cc469138082cbe7a072b4b147ac50d88bba7c
Mikail Khan <mikail.khan@digital.hmrc.gov.uk>
2021-05-07 15:16:16 +0100

    CDSR-568: migrate to Play 2.8

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
   val silencerVersion = "1.7.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-27"  % "3.2.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28"  % "5.2.0",
     "uk.gov.hmrc"       %% "stub-data-generator"        % "0.5.3",
     "com.eclipsesource" %% "play-json-schema-validator" % "0.9.5",
     "org.julienrf"      %% "play-json-derived-codecs"  % "7.0.0",
