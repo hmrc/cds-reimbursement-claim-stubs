5bd3f2df70f36db2c5cf54d883a5e2e32978e732
Artur Opala <250927+arturopala@users.noreply.github.com>
2023-07-25 04:14:16 +0100

    change BARS address requirement to optional

--- app/uk/gov/hmrc/cdsreimbursementclaimstubs/models/bankaccountreputation/request/BarsSubject.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaimstubs/models/bankaccountreputation/request/BarsSubject.scala
@@ -24,7 +24,7 @@ final case class BarsSubject(
   firstName: Option[String], // Must be between 1 and 35 characters long
   lastName: Option[String], // Must be between 1 and 35 characters long
   dob: Option[String], // date of birth: ISO-8601 YYYY-MM-DD
-  address: BarsAddress
+  address: Option[BarsAddress]
 ) {
   def getAccountName: Option[String] = {
     val accountName = Seq(title, name, firstName, lastName)
@@ -39,5 +39,5 @@ object BarsSubject {
   implicit val format: OFormat[BarsSubject] = Json.format[BarsSubject]
 
   def apply(address: BarsAddress): BarsSubject =
-    BarsSubject(None, None, None, None, None, address)
+    BarsSubject(None, None, None, None, None, Some(address))
 }
