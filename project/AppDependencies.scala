76383042386f2ef5ea79ba9f44ee0c30ac28994c
Artur Opala <250927+arturopala@users.noreply.github.com>
2024-01-18 09:33:40 +0000

    [CDSR-2733] upgrade to use Play Framework 3

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,19 +2,19 @@ import sbt._
 
 object AppDependencies {
   val compile = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-frontend-play-28" % "7.22.0",
-    "uk.gov.hmrc"            %% "play-frontend-hmrc"         % "7.21.0-play-28",
-    "uk.gov.hmrc"            %% "bootstrap-backend-play-28"  % "7.22.0",
+    "uk.gov.hmrc"            %% "bootstrap-frontend-play-30" % "8.4.0",
+    "uk.gov.hmrc"            %% "bootstrap-backend-play-30"  % "8.4.0",
+    "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30" % "8.3.0",
     "uk.gov.hmrc"            %% "stub-data-generator"        % "1.1.0",
     "com.eclipsesource"      %% "play-json-schema-validator" % "0.9.5",
     "org.julienrf"           %% "play-json-derived-codecs"   % "7.0.0",
-    "org.typelevel"          %% "cats-core"                  % "2.9.0",
-    "org.scala-lang.modules" %% "scala-xml"                  % "2.0.0-M4"
+    "org.typelevel"          %% "cats-core"                  % "2.10.0",
+    "org.scala-lang.modules" %% "scala-xml"                  % "2.2.0"
   )
 
   val test = Seq(
-    "org.scalatest"          %% "scalatest"          % "3.2.11" % Test,
-    "com.vladsch.flexmark"    % "flexmark-all"       % "0.62.2" % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0"  % Test
+    "org.scalatest"          %% "scalatest"          % "3.2.17" % Test,
+    "com.vladsch.flexmark"    % "flexmark-all"       % "0.64.8" % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play" % "7.0.1"  % Test
   )
 }
