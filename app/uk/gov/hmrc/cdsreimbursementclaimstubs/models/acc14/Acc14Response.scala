5d291a4a22078f07f9631d0dca8ae8a22a7a4c97
Mathew May <29102292+mghmay@users.noreply.github.com>
2024-07-19 14:16:15 +0100

    [CDSR-3142] Remove declarantReferenceNumber from 01AAAAAAAAAAAAAAA1 securities to allow for testing LRN display

--- app/uk/gov/hmrc/cdsreimbursementclaimstubs/models/acc14/Acc14Response.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaimstubs/models/acc14/Acc14Response.scala
@@ -1373,7 +1373,6 @@ object Acc14Response {
         |        {
         |            "declarationId": "$declarationId",
         |            "acceptanceDate": "2019-08-13",
-        |            "declarantReferenceNumber": "XFGLKJDSE5GDPOIJEW985T",
         |            "securityReason": "$reasonForSecurity",
         |            "btaDueDate": "2019-09-13",
         |            "procedureCode": "71",
