c8d8f114af4fffb99ddbe5485ef6e4eb0c1a8b10
andrew-bintcliffe <186038219+andrew-bintcliffe@users.noreply.github.com>
2026-04-20 10:51:16 +0100

    DCWL-4566 | Update Dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,8 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30" % "12.32.0",
     "uk.gov.hmrc"            %% "play-json-schema-validator" % "0.1.0",
     "org.typelevel"          %% "cats-core"                  % "2.13.0",
-    "org.scala-lang.modules" %% "scala-xml"                  % "2.4.0"
+    "org.scala-lang.modules" %% "scala-xml"                  % "2.4.0",
+    "org.scalacheck"         %% "scalacheck"                 % "1.19.0"
   )
 
   val test = Seq(
@@ -17,7 +18,6 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "stub-data-generator"    % "1.6.0"          % Test exclude ("org.scalacheck", "scalacheck_3"),
     "org.scalatest"          %% "scalatest"              % "3.2.19"         % Test,
     "com.vladsch.flexmark"    % "flexmark-all"           % "0.64.8"         % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.2"          % Test,
-    "org.scalacheck"         %% "scalacheck"             % "1.17.0"         % Test
+    "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.2"          % Test
   )
 }
