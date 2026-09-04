a16a82cdc8a27b0ea8176274d4f4b29920b66050
tomndennis <tomndennis@users.noreply.github.com>
2026-09-04 13:23:09 +0100

    DCWL-5017 - Update app dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,15 +9,15 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30" % "13.13.0",
     "uk.gov.hmrc"            %% "play-json-schema-validator" % "0.1.0",
     "org.typelevel"          %% "cats-core"                  % "2.13.0",
-    "org.scala-lang.modules" %% "scala-xml"                  % "2.4.0"
+    "org.scala-lang.modules" %% "scala-xml"                  % "2.4.0",
+    "org.scalacheck"         %% "scalacheck"                 % "1.20.0"
   )
 
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion % Test,
     "uk.gov.hmrc"            %% "stub-data-generator"    % "1.6.0"          % Test exclude ("org.scalacheck", "scalacheck_3"),
     "org.scalatest"          %% "scalatest"              % "3.2.19"         % Test,
-    "com.vladsch.flexmark"   % "flexmark-all"            % "0.64.8"         % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.2"          % Test,
-    "org.scalacheck"         %% "scalacheck"             % "1.20.0"        % Test
+    "com.vladsch.flexmark"    % "flexmark-all"           % "0.64.8"         % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.2"          % Test
   )
 }
