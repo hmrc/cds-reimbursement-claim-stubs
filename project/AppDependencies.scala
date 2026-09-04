f567b56e4e052d5ddc28e7c7eb46f1ed527e9f4d
Tom Dennis <207199298+tomndennis@users.noreply.github.com>
2026-09-04 15:12:16 +0100

    DCWL-5017 - Add serviceNowMapping entry to repository.yaml (#206)
    * DCWL-5017 - Add serviceNowMapping entry to repository.yaml
    
    * DCWL-5017 - Update app dependencies
    
    * DCWL-5017 - Update app dependencies
    
    * DCWL-5017 - Update app dependencies
    
    ---------
    
    Co-authored-by: tomndennis <tomndennis@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,16 +1,16 @@
 import sbt._
 
 object AppDependencies {
-  val bootstrapVersion = "10.7.0"
+  val bootstrapVersion = "10.8.0"
 
   val compile = Seq(
     "uk.gov.hmrc"            %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"            %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30" % "12.32.0",
+    "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30" % "13.13.0",
     "uk.gov.hmrc"            %% "play-json-schema-validator" % "0.1.0",
     "org.typelevel"          %% "cats-core"                  % "2.13.0",
     "org.scala-lang.modules" %% "scala-xml"                  % "2.4.0",
-    "org.scalacheck"         %% "scalacheck"                 % "1.19.0"
+    "org.scalacheck"         %% "scalacheck"                 % "1.20.0"
   )
 
   val test = Seq(
