b4d6b6a68bfd6951e09f07cf2547a1b69adba515
tomndennis <tomndennis@users.noreply.github.com>
2026-09-04 11:32:01 +0100

    DCWL-5017 - Update app dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"            %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"            %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30" % "12.32.0",
+    "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30" % "13.13.0",
     "uk.gov.hmrc"            %% "play-json-schema-validator" % "0.1.0",
     "org.typelevel"          %% "cats-core"                  % "2.13.0",
     "org.scala-lang.modules" %% "scala-xml"                  % "2.4.0",
